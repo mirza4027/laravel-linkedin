222297c15194e4d437876adaeaad5df66e29e19a
Abdul Ahad Mirza <ahadsays89@gmail.com>
2016-05-02 17:40:38 +0500

    added additional fields to the form

--- resources/views/auth/register_step_2.blade.php
+++ resources/views/auth/register_step_2.blade.php
@@ -28,62 +28,74 @@
                             </div>
                         </div>
                         
-                        <div class="form-group{{ $errors->has('lname') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Last Name</label>
+                        <div class="form-group{{ $errors->has('postal_code') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Postal Code</label>
 
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="lname" value="{{ old('lname') }}">
+                                <input type="text" class="form-control" name="postal_code" value="{{ old('postal_code') }}">
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('postal_code'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('lname') }}</strong>
+                                        <strong>{{ $errors->first('postal_code') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Title</label>
 
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                <input type="email" class="form-control" name="title" value="{{ old('title') }}">
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('title'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('title') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
+                        <div class="form-group{{ $errors->has('company') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Company</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
+                                <input type="text" class="form-control" name="company">
 
-                                @if ($errors->has('password'))
+                                @if ($errors->has('company'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                        <strong>{{ $errors->first('company') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirm Password</label>
+                        <div class="form-group{{ $errors->has('currently') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">I am currently:</label>
 
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
+                            <div class="col-md-2 radio">
+                                <input class="radio_box" type="radio" name="job_type" value="0" id="job_type" onclick="employed_options()" checked> Employed
+                                
+<!--                                @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
                                     </span>
-                                @endif
+                                @endif-->
+                            </div>
+                            <div class="col-md-2 radio">
+                                <input class="radio_box" type="radio" name="job_type" value="1" id="job_type" onclick="job_seeker_options()"> Job Seeker
+                            </div>
+                            <div class="col-md-2 radio">
+                                <input class="radio_box" type="radio" name="job_type" value="2" id="job_type" onclick="student_options()"> Student
                             </div>
                         </div>
-
+                        <div class="form-group{{ $errors->has('self_emp') ? ' has-error' : '' }}">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-6 radio">
+                                <input class="check_box" type="checkbox" name="self_employee" value="1">I am self-employed
+                            </div>
+                            
+                        </div>    
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
